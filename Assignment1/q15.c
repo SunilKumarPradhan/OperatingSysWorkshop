79825ab96ba1741c7e0296f6c4f21120c9cdc124
SunilKumarPradhan <89890497+SunilKumarPradhan@users.noreply.github.com>
2024-01-11 17:16:22 +0530

    re ran the programs just before endsems

--- Assignment1/q15.c
+++ Assignment1/q15.c
@@ -2,10 +2,13 @@
 
 int main() {
     int x = 2, y = 3, z = 4;
+    int x1;
     
     x *= -2 * (y + z) / 3;
+    x1 = x * -2 * (y + z) / 3;
     
     printf("The value of the expression is: %d\n", x);
+    printf("The value of the expression is: %d\n", x1);
     
     return 0;
 }
