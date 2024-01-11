35d1ef97d5f1b04244c7438e21f909319f577377
SunilKumarPradhan <89890497+SunilKumarPradhan@users.noreply.github.com>
2024-01-11 13:35:06 +0530

    updated the codes

--- AssignmentS02/q10.c
+++ AssignmentS02/q10.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <pthread.h>
-#include <semaphore.h>
+// #include <pthread.h>
+// #include <semaphore.h>
 #include <unistd.h>
 
 #define N 5 // Number of chairs in the waiting room
